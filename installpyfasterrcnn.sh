6dfeffee354b2baf2c0a12bacd21c8463d333a8a
teler <teler@mail.ru>
2016-07-04 03:09:45 +0300

    Update installpyfasterrcnn.sh

--- installpyfasterrcnn.sh
+++ installpyfasterrcnn.sh
@@ -10,9 +10,10 @@ git clone git://github.com/xianyi/OpenBLAS
 cd OpenBLAS
 make FC=gfortran
 sudo make PREFIX=/usr/local/ install
-
+cd ~
 git clone --recursive https://github.com/rbgirshick/py-faster-rcnn.git
-wget 
+cd py-faster-rcnn
+wget https://raw.githubusercontent.com/teler/install-py-rcnn-on-jetson-tk1/master/Makefile.config
 
 
 
