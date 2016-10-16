ca99c4b8b4884f5f54283193b86b59f77dbf2163
teler <teler@mail.ru>
2016-10-16 18:28:08 +0300

    Update caffe-nv-v12-TK1.sh

--- caffe-nv-v12-TK1.sh
+++ caffe-nv-v12-TK1.sh
@@ -17,45 +17,26 @@ libboost-thread-dev libboost-system-dev \
 libatlas-base-dev libhdf5-serial-dev libgflags-dev \
 libgoogle-glog-dev liblmdb-dev -y
 
-
+sudo apt-get install python-numpy
 
 sudo usermod -a -G video $USER
 
-
-
 export CAFFE_ROOT=~/caffe
-git clone --branch v0.12.0 https://github.com/NVIDIA/caffe.git $CAFFE_ROOT
-
-cd CAFFE_ROOT && git checkout dev
-cp Makefile.config.example Makefile.config
-sed -i '/^# USE_CUDNN := 1/s/^# //' Makefile.config
-sed -i '/^# WITH_PYTHON_LAYER := 1/s/^# //' Makefile.config
-#sed -i '/^PYTHON_INCLUDE/a    /usr/local/lib/python2.7/dist-packages/numpy/core/include/ \\' Makefile.config
-
-make -j 4 all
-
-make -j 4 runtest
-
-build/tools/caffe time --model=models/bvlc_alexnet/deploy.prototxt --gpu=0
-
-
-
+git clone --branch v0.12.0 https://github.com/teler/caffe.git $CAFFE_ROOT 
 
 sudo pip install -r $CAFFE_ROOT/python/requirements.txt
 cat $CAFFE_ROOT/python/requirements.txt | xargs -n1 sudo pip install
 cd $CAFFE_ROOT
 
+#edited for jetson tk1 caffe-nv ver 0.12.0 https://github.com/NVIDIA/caffe.git --branch v0.12.0
+
+cd CAFFE_ROOT && git checkout dev
 cp Makefile.config.example Makefile.config
-#run without cudnn
 
+#sed -i '/^PYTHON_INCLUDE/a    /usr/local/lib/python2.7/dist-packages/numpy/core/include/ \\' Makefile.config
+make clean
+make -j 4 all
+make -j 4 py
+make -j 4 runtest
 
-
-#/home/oleg/tegra/caffe/src/caffe/util
-#const size_t LMDB_MAP_SIZE = 1099511627776;  // 1 TB
-#536870912
-
-mkdir build &&
-cd build &&
-cmake ..
-
-make --jobs=4
+build/tools/caffe time --model=models/bvlc_alexnet/deploy.prototxt --gpu=0
